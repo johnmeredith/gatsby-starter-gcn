46961257cde5e70caf5311ab83fd8286f6c7c5a0
John Meredith <johnmeredith@users.noreply.github.com>
2018-11-30 18:19:20 +0000

    Added disqus component

---
+++ src/components/Disqus.js
@@ -0,0 +1,28 @@
+import Disqus from 'disqus-react'
+
+class BlogPostTemplate extends Component {
+  render() {
+    const post = this.props.data.markdownRemark
+    const disqusConfig = {
+      url: `https://www.xiaoru.li${this.props.location.pathname}`,
+      identifier: `${this.props.location.pathname}`,
+      title: post.frontmatter.title,
+    }
+
+    // Below is the version that blew the comment section up...
+
+    // const disqusConfig = {
+    //   url: `https://www.xiaoru.li${this.props.location}`,
+    //   identifier: this.props.location,
+    //   title: post.frontmatter.title,
+    // }
+
+    return (
+      <Layout>
+        {/* blah blah blah */}
+        <Disqus.DiscussionEmbed shortname="health-archetextures" config={disqusConfig} />
+        {/* blah blah blah */}
+      </Layout>
+    )
+  }
+}

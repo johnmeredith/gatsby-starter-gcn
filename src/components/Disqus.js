b9ffffbf3f59971809df2b495cf475df022e038a
John Meredith <johnmeredith@users.noreply.github.com>
2018-11-30 18:21:32 +0000

    Update Disqus.js

--- src/components/Disqus.js
+++ src/components/Disqus.js
@@ -4,7 +4,7 @@ class BlogPostTemplate extends Component {
   render() {
     const post = this.props.data.markdownRemark
     const disqusConfig = {
-      url: `https://www.xiaoru.li${this.props.location.pathname}`,
+      url: `https://www.archetextur.es/${this.props.location.pathname}`,
       identifier: `${this.props.location.pathname}`,
       title: post.frontmatter.title,
     }

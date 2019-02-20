61957a88148e155c2845fb772b731b301861709e
John Meredith <johnmeredith@users.noreply.github.com>
2019-02-20 21:10:40 +0000

    Update siteConfig.js
    Amedned site URL for SEO.

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,10 +3,10 @@ module.exports = {
   siteTitleAlt: 'Health Archetextures', // This allows an alternative site title for SEO schema.
   publisher: 'Publisher named John Meredith', // Organization name used for SEO schema
   siteDescription:
-    'Health informatics interoperababble...',
-  siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
-  postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
-  postsPerPage: 6, // Number of posts shown on paginated pages
+    'Health informatics interoperababble... Interoperability, OpenEHR, FHIR, Snomed, clinical modelling and other musings.',
+  siteUrl: 'https://www.archetextur.es', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  postsPerHomePage: 10, // Number of posts shown on the 1st page of of the index.js template (home page)
+  postsPerPage: 9, // Number of posts shown on paginated pages
   author: 'John Meredith', // Author for RSS author segment and SEO schema
   authorUrl: 'https://www.archetextur.es/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@johnmeredith', // Change for Twitter Cards
@@ -17,5 +17,5 @@ module.exports = {
   siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
   backgroundColor: '#e9e9e9', // Used for Offline Manifest
   themeColor: '#121212', // Used for Offline Manifest
-  copyright: 'Content © 2018 John Meredith', // Copyright string for the RSS feed
+  copyright: 'Content © 2019 John Meredith', // Copyright string for the RSS feed
 }
